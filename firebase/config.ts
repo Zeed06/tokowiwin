94d4cba29063c8c4af4f47af3fac2521ff4a1813
Zeed06 <botakgantengg12@gmail.com>
2026-03-24 14:43:05 +0700

    setup env

--- firebase/config.ts
+++ firebase/config.ts
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAjXDZApty6SCYsxsnTQ3nrGWNA0G87oYc",
-  authDomain: "tokowiwin-5c5d8.firebaseapp.com",
-  projectId: "tokowiwin-5c5d8",
-  storageBucket: "tokowiwin-5c5d8.firebasestorage.app",
-  messagingSenderId: "26186479074",
-  appId: "1:26186479074:web:2cd4b1d7b358b7c352b5bb"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
